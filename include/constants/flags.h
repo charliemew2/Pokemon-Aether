360e518ff03f50a3bb091c5327e2cae92fbc568f
charliemew2 <charliemew2@gmail.com>
2026-02-03 14:25:40 -0600

    Added Sayo Level Caps!  Also added the rest of the first pass of all the encounters in split 1!

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1507,8 +1507,8 @@
 
 #define FLAG_RECEIVED_POKEDEX_FROM_BIRCH            (SYSTEM_FLAGS + 0x84)
 
-#define FLAG_UNUSED_0x8E5                           (SYSTEM_FLAGS + 0x85) // Unused Flag
-#define FLAG_UNUSED_0x8E6                           (SYSTEM_FLAGS + 0x86) // Unused Flag
+#define FLAG_DEFEATED_SAYO                          (SYSTEM_FLAGS + 0x85) // UNUSED FLAG CHANGED TO SAYO LEVEL CAP
+#define FLAG_DEFEATED_KRISTINE                      (SYSTEM_FLAGS + 0x86) // UNUSED FLAG CHANGED TO KRISTINE LEVEL CAP
 #define FLAG_UNUSED_0x8E7                           (SYSTEM_FLAGS + 0x87) // Unused Flag
 #define FLAG_UNUSED_0x8E8                           (SYSTEM_FLAGS + 0x88) // Unused Flag
 #define FLAG_UNUSED_0x8E9                           (SYSTEM_FLAGS + 0x89) // Unused Flag
--- src/caps.c
+++ src/caps.c
@@ -9,9 +9,10 @@ u32 GetCurrentLevelCap(void)
 {
     static const u32 sLevelCapFlagMap[][2] =
     {
-        {FLAG_BADGE01_GET, 13},
-        {FLAG_BADGE02_GET, 19},
-        {FLAG_BADGE03_GET, 24},
+        {FLAG_DEFEATED_SAYO, 14},
+        {FLAG_DEFEATED_KRISTINE, 19},
+        {FLAG_BADGE02_GET, 25},
+        {FLAG_BADGE03_GET, 27},
         {FLAG_BADGE04_GET, 29},
         {FLAG_BADGE05_GET, 31},
         {FLAG_BADGE06_GET, 33},
